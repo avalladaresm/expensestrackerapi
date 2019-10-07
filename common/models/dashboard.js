03c0f9f459a2a1d504d9115ee53204214e7956f9
Alejandro Valladares <alejo.valladares14@gmail.com>
2019-10-07 16:13:44 -0600

    Endpoint created to get data for pie chart

--- common/models/dashboard.js
+++ common/models/dashboard.js
@@ -12,4 +12,16 @@ module.exports = function(Dashboard) {
 		returns: { root: true, type: 'object' },
 		http: { path: '/getLastRecords', verb: 'get' }
 	});
+	
+	Dashboard.getExpensesByCategory = function( next) {
+		Dashboard.app.dataSources.expensestrackerdb.connector.execute(
+			'EXEC GetExpensesByCategory',
+			next
+		);
+	};
+
+	Dashboard.remoteMethod('getExpensesByCategory', {
+		returns: { root: true, type: 'object' },
+		http: { path: '/getExpensesByCategory', verb: 'get' }
+	});
 };
